8d7469ae1834e4159c50bf4020b5091ec7745f10
Rodrigo Arce <albo@pragmore.com>
2014-08-28 02:12:02 -0300

    Creamos el controlador Admin

---
+++ module/Application/src/Application/Controller/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AdminController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
