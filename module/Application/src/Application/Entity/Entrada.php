d29f935e8b2f482dd6b12c6f0171300610edfb98
Rodrigo Arce <albo@pragmore.com>
2014-09-03 15:00:05 -0300

    Nosotros podríamos haberlas creado a mano, no es absolutamente necesario pero es útil.

--- module/Application/src/Application/Entity/Entrada.php
+++ module/Application/src/Application/Entity/Entrada.php
@@ -17,4 +17,60 @@ class Entrada
     protected $titulo;
     /** @ORM\Column */
     protected $contenido;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     * @return Entrada
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string 
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set contenido
+     *
+     * @param string $contenido
+     * @return Entrada
+     */
+    public function setContenido($contenido)
+    {
+        $this->contenido = $contenido;
+
+        return $this;
+    }
+
+    /**
+     * Get contenido
+     *
+     * @return string 
+     */
+    public function getContenido()
+    {
+        return $this->contenido;
+    }
 }
