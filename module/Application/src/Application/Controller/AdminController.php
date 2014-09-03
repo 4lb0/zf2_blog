90bb640587243c9b7db06e5e767a780c7de61bb9
Rodrigo Arce <albo@pragmore.com>
2014-09-03 15:47:26 -0300

    Listamos las entradas y las guardamos, por ahora sin ninguna validación.

--- module/Application/src/Application/Controller/AdminController.php
+++ module/Application/src/Application/Controller/AdminController.php
@@ -5,18 +5,44 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Application\Form\EntradaForm;
+use Application\Entity\Entrada;
 
 class AdminController extends AbstractActionController
 {
 
     public function indexAction()
     {
-        return new ViewModel();
+        $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        // Le pedimos al EntityManager que nos de el repositorio de entrada
+        // El repositorio es el objeto al cual le pedimos los datos que estan 
+        // en la base
+        $repositorio = $em->getRepository('Application\Entity\Entrada');
+        // Para este primer caso obtenemos todas las entradas sin ningún criterio
+        // u orden particular
+        $entradas = $repositorio->findAll();
+        return new ViewModel(['entradas' => $entradas]);
     }
 
     public function nuevoAction()
     {
         $form = new EntradaForm();
+        if ($this->request->isPost()) {
+            $data = $this->request->getPost();
+
+            // por ahora seteamos los datos manualmente
+            $entrada = new Entrada();
+            $entrada->setTitulo($data->titulo); 
+            $entrada->setContenido($data->contenido);
+                        
+            $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+            // le decimos al EntityManager que vamos a guardar el objeto entrada
+            $em->persist($entrada);
+            // aca ejecuta en la base todos los cambios que le pedímos
+            $em->flush();
+
+            // redirigimos al listado
+            return $this->redirect()->toRoute('admin');
+        }
         return new ViewModel(['form' => $form]);
     }
 
